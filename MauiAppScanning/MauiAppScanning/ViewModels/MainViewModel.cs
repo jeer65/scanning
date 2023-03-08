540e250e28468ca1c9eec6113f907775d5bbd0f1
C. Stacy Harris III <stacyh@microsoft.com>
2023-03-08 16:39:15 -0500

    Barcode parsing

--- MauiAppScanning/MauiAppScanning/ViewModels/MainViewModel.cs
+++ MauiAppScanning/MauiAppScanning/ViewModels/MainViewModel.cs
@@ -1,20 +1,36 @@
-﻿namespace MauiAppScanning.ViewModels;
+﻿using IdParser;
+
+
+namespace MauiAppScanning.ViewModels;
 
 public partial class MainViewModel : BaseViewModel
 {
     private RosterDatabase _rosterDatabase;
 
     [ObservableProperty]
-    public ObservableCollection<Roster> rosters = new();
+    private ObservableCollection<Roster> rosters = new();
 
     [ObservableProperty]
-    Roster selectedRoster;
+    private string barcodeData;
+
+    [ObservableProperty]
+    IdentificationCard idCard;
 
     public MainViewModel(RosterDatabase rosterDatabase)
     {
         _rosterDatabase = rosterDatabase;
     }
 
+    [RelayCommand]
+    public void ParseBarcode()
+    {
+        BarcodeData = "@\n\rANSI 636032030102DL00410218ZM02590027DLDCA\nDCB\nDCD\nDBA12122023\nDCSHARRIS\nDCTCHARLES STACY\nDBD11212019\nDBB12121963\nDBC1\nDAY\nDAU\nDAG2051 HOLLOW OAK DR\nDAIANN ARBOR\nDAJMI\nDAK481038402  \nDAQH 620 115 777 944\nDCF\nDCG\nDCH\nDAH\nDCKH620115777944196312122023\nDDAF\n\rZMZMARev 01-21-2011\nZMB01\n\r";
+        
+        if (!string.IsNullOrWhiteSpace( BarcodeData))
+        {
+            IdCard = Barcode.Parse(BarcodeData);
+        }
+    }
 
     [RelayCommand]
     public async Task LoadFile()
