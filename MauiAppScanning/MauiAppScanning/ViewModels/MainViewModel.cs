1a992f64b650f9cfeed3072360e24a7a3f9403a4
C. Stacy Harris III <stacyh@microsoft.com>
2023-03-08 19:44:01 -0500

    Scan Test

--- MauiAppScanning/MauiAppScanning/ViewModels/MainViewModel.cs
+++ MauiAppScanning/MauiAppScanning/ViewModels/MainViewModel.cs
@@ -23,10 +23,9 @@ public partial class MainViewModel : BaseViewModel
 
     [RelayCommand]
     public void ParseBarcode()
-    {
-        BarcodeData = "@\n\rANSI 636032030102DL00410218ZM02590027DLDCA\nDCB\nDCD\nDBA12122023\nDCSHARRIS\nDCTCHARLES STACY\nDBD11212019\nDBB12121963\nDBC1\nDAY\nDAU\nDAG2051 HOLLOW OAK DR\nDAIANN ARBOR\nDAJMI\nDAK481038402  \nDAQH 620 115 777 944\nDCF\nDCG\nDCH\nDAH\nDCKH620115777944196312122023\nDDAF\n\rZMZMARev 01-21-2011\nZMB01\n\r";
+    {        
         
-        if (!string.IsNullOrWhiteSpace( BarcodeData))
+        if (!string.IsNullOrWhiteSpace(BarcodeData))
         {
             IdCard = Barcode.Parse(BarcodeData);
         }
